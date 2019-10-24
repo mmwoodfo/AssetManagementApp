088aabf91d05f73b6864a314949189a883de0682
mmwoodfo <mmwoodfo@asu.edu>
2019-10-24 09:50:31 -0700

    Start of GUI layout with segues

---
+++ InventoryApp/InventoryApp/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InventoryApp
+//
+//  Created by Meghan on 10/23/19.
+//  Copyright © 2019 Herberger IT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
